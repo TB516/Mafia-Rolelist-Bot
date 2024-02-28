ba96980f6f4fb5fcb9f33a8103a6207ea2db6290
Thomas <tberrios9800@gmail.com>
2024-02-28 13:06:21 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using DSharpPlus.SlashCommands;
+using DSharpPlus;
+
+namespace Mafia_Game_Rolelist_Bot
+{
+    internal class Program
+    {
+        public static DiscordClient discord = new DiscordClient(new DiscordConfiguration()
+        {
+            Token = File.ReadAllLines("BotKey.txt")[0],
+            TokenType = TokenType.Bot,
+            Intents = DiscordIntents.AllUnprivileged
+        });
+
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+        static async Task MainAsync()
+        {
+            SlashCommandsExtension slash = discord.UseSlashCommands();
+
+            await discord.ConnectAsync();
+            Console.WriteLine("Connected");
+            while (Console.ReadLine() != "stop") ;
+        }
+    }
+}
