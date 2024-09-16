2ce856e5a29aaa89676089ef903be68d73ad8dbe
TB516 <tberrios9800@gmail.com>
2024-09-16 14:12:42 -0400

    Modified link picker code

--- RoleDeckComponents/RoledeckMessage.cs
+++ RoleDeckComponents/RoledeckMessage.cs
@@ -33,7 +33,7 @@ namespace Mafia_Bot.RoleDeckComponents
 
             _messageBuilder = new();
 
-            _messageBuilder.WithContent($"# \"{FormatString(_deck.Name)}\" By {author}\n\n{GetRolelist()}{GetPhaseTimes()}{GetBannedRoles()}");
+            _messageBuilder.WithContent($"# \"{FormatString(_deck.Name)}\" By {author}\n\n{GetRolelist()}{GetPhaseTimes()}{GetEnabledRoles()}");
 
             _messageBuilder.AddFile($"{_deck.Name}.json", new MemoryStream(Encoding.UTF8.GetBytes(_deck.JsonString)));
         }
@@ -56,26 +56,13 @@ namespace Mafia_Bot.RoleDeckComponents
                 {
                     string roleset = FormatString(_deck.Rolelist[i][j].ToString());
 
-                    switch (roleset)
+                    if (Roledeck.RoleTypes.Contains(_deck.Rolelist[i].ToString()))
                     {
-                        case "Town":
-                        case "Mafia":
-                        case "Neutral":
-                        case "Fiends":
-                        case "Cult":
-                        case "Town Investigative":
-                        case "Town Killing":
-                        case "Town Support":
-                        case "Town Protective":
-                        case "Mafia Killing":
-                        case "Mafia Support":
-                        case "Neutral Evil":
-                        case "Any":
-                            rolelist += $"[{roleset}]({s_wikiLink}/generated/role_set)" + ((_deck.Rolelist[i].Length > 1 && j < _deck.Rolelist[i].Length - 1) ? " OR " : "");
-                            break;
-                        default:
-                            rolelist += $"[{roleset}]({s_wikiLink}/role/{_deck.Rolelist[i][j].ToLower()})" + ((_deck.Rolelist[i].Length > 1 && j < _deck.Rolelist[i].Length - 1) ? " OR " : "");
-                            break;
+                        rolelist += $"[{roleset}]({s_wikiLink}/generated/role_set)" + ((_deck.Rolelist[i].Length > 1 && j < _deck.Rolelist[i].Length - 1) ? " OR " : "");
+                    }
+                    else
+                    {
+                        rolelist += $"[{roleset}]({s_wikiLink}/role/{_deck.Rolelist[i][j].ToLower()})" + ((_deck.Rolelist[i].Length > 1 && j < _deck.Rolelist[i].Length - 1) ? " OR " : "");
                     }
                 }
                 rolelist += "\n";
@@ -94,37 +81,13 @@ namespace Mafia_Bot.RoleDeckComponents
 
             return phaseTimes;
         }
-        private string GetBannedRoles()
+        private string GetEnabledRoles()
         {
             string enabledRoles = "## Enabled Roles\n";
 
             for (int i = 0; i < _deck.EnabledRoles.Length; i++)
             {
-                string roleset = FormatString(_deck.EnabledRoles[i].ToString());
-
-                switch (roleset)
-                {
-                    case "Town":
-                    case "Mafia":
-                    case "Neutral":
-                    case "Fiends":
-                    case "Cult":
-                        enabledRoles += $"- [{roleset}]({s_wikiLink}/standard/{_deck.EnabledRoles[i].ToLower()})\n";
-                        break;
-                    case "Town Investigative":
-                    case "Town Killing":
-                    case "Town Support":
-                    case "Town Protective":
-                    case "Mafia Killing":
-                    case "Mafia Support":
-                    case "Neutral Evil":
-                    case "Any":
-                        enabledRoles += $"- [{roleset}]({s_wikiLink}/generated/role_set)\n";
-                        break;
-                    default:
-                        enabledRoles += $"- [{roleset}]({s_wikiLink}/role/{_deck.EnabledRoles[i].ToLower()})\n";
-                        break;
-                }
+                enabledRoles += $"- [{FormatString(_deck.EnabledRoles[i].ToString())}]({s_wikiLink}/role/{_deck.EnabledRoles[i].ToLower()})\n";
             }
 
             enabledRoles += (_deck.EnabledRoles.Length == 0) ? "- None" : "";
